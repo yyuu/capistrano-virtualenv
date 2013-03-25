62fd187c189a87669f18d7b236b9e3d3e961f69d
Yamashita Yuu <yamashita@geishatokyo.com>
2013-03-25 17:10:36 +0900

    add tests

--- test/config/deploy.rb
+++ test/config/deploy.rb
@@ -17,6 +17,14 @@ role :db,  "192.168.33.10", :primary => true
 $LOAD_PATH.push(File.expand_path("../../lib", File.dirname(__FILE__)))
 require "capistrano-virtualenv"
 
+def _invoke_command(cmdline, options={})
+  if options[:via] == :run_locally
+    run_locally(cmdline)
+  else
+    invoke_command(cmdline, options)
+  end
+end
+
 def assert_file_exists(file, options={})
   begin
     _invoke_command("test -f #{file.dump}", options)
@@ -65,6 +73,8 @@ def reset_virtualenv!
 end
 
 def uninstall_virtualenv!
+  run("rm -rf #{virtualenv_shared_path.dump}")
+  run("rm -rf #{virtualenv_release_path.dump}")
 end
 
 task(:test_all) {
@@ -86,12 +96,48 @@ namespace(:test_default) {
 
   task(:setup) {
     uninstall_virtualenv!
+    set(:virtualenv_requirements, %w(simplejson))
+    reset_virtualenv!
     find_and_execute_task("deploy:setup")
-    find_and_execute_task("deploy")
   }
 
   task(:teardown) {
-    uninstall_virtualenv!
+  }
+
+  task(:test_deploy) {
+    assert_command("#{virtualenv_shared_python} --version")
+    assert_command_fails("echo null | #{virtualenv_shared_python} -m simplejson.tool")
+    find_and_execute_task("deploy")
+    assert_command("#{virtualenv_release_python} --version")
+    assert_command("echo null | #{virtualenv_release_python} -m simplejson.tool")
+    assert_command("#{virtualenv_current_python} --version")
+    assert_command("echo null | #{virtualenv_current_python} -m simplejson.tool")
+  }
+
+  task(:test_redeploy) {
+    assert_command("#{virtualenv_shared_python} --version")
+    assert_command("echo null | #{virtualenv_shared_python} -m simplejson.tool")
+    variables.each_key do |key|
+      reset!(key)
+    end
+    find_and_execute_task("deploy")
+    assert_command("#{virtualenv_release_python} --version")
+    assert_command("echo null | #{virtualenv_release_python} -m simplejson.tool")
+    assert_command("#{virtualenv_current_python} --version")
+    assert_command("echo null | #{virtualenv_current_python} -m simplejson.tool")
+  }
+
+  task(:test_rollback) {
+    assert_command("#{virtualenv_shared_python} --version")
+    assert_command("echo null | #{virtualenv_shared_python} -m simplejson.tool")
+    variables.each_key do |key|
+      reset!(key)
+    end
+    find_and_execute_task("deploy:rollback")
+    assert_command_fails("#{virtualenv_release_python} --version")
+    assert_command_fails("echo null | #{virtualenv_release_python} -m simplejson.tool")
+    assert_command("#{virtualenv_current_python} --version")
+    assert_command("echo null | #{virtualenv_current_python} -m simplejson.tool")
   }
 }
 
